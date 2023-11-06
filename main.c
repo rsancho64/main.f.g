93dcae3818dff054bf36eaeb82dcd3f52309e29d
rsancho64 <rsancho@iesrioarba.es>
2023-11-06 11:47:19 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,20 @@
+int masDos(int x)
+{
+    int localg = x + 2;
+    return localg;
+}
+
+int porDos(int y)
+{
+    localf = masDos(y);
+    return localf;
+}
+
+int main(void)
+{
+
+    int localmain = 11;
+    localmain = porDos(masDos(localmain));    
+    printf("valor final: %d\n", localmain);
+
+}
